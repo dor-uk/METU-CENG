ded3a19950b4f19f147fe65a4119b6897060d923
dor-uk <118803335+dor-uk@users.noreply.github.com>
2022-12-12 15:28:10 +0300

    Create Window.h

---
+++ Ceng213/The1/Window.h
@@ -0,0 +1,33 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "LinkedList.h"
+#include "Tab.h"
+
+class Window
+{
+public:
+    Window();
+
+    Tab getActiveTab();
+    bool isEmpty() const;
+
+    void newTab(const Tab &tab);
+    void closeTab();
+    void moveActiveTabTo(int index);
+    void changeActiveTabTo(int index);
+
+    void addTab(Node<Tab> &tab);
+
+    void print();
+    
+private:
+    LinkedList<Tab> tabs;
+    int activeTab;
+};
+
+#endif
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
